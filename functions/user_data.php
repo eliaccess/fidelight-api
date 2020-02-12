a5194be7ca29b37fa33a119aa76e3ee29ec91aa7
Elias Limouni <elias.limouni@gmail.com>
2020-02-12 13:38:36 +0100

    Adding the management of the hashed passwords and the salts for the users connection :key:

--- functions/user_data.php
+++ functions/user_data.php
@@ -12,6 +12,10 @@ function get_user_profile($login, $password, $database){
      *  SQL Query :
      *  FROM user, (SELECT name FROM user_type WHERE id IN (SELECT user_type FROM user WHERE user.login=$login)) AS user_type WHERE user.login=$login;
      */
+    //Verify the password
+    $password = get_hash_pwd($login, $password, $database);
+
+    //get the data
     $datas = $database->select("user",
         [
             "[>]user_type" => ["user_type"=>"id"]
@@ -21,14 +25,36 @@ function get_user_profile($login, $password, $database){
             "user.login",
             "user.mail",
             "user.registration_date",
+            "user.birthdate",
             "user_type.name (user_type)"
         ], ["login"=>$login, "hash_pwd" => $password]);
 
-    if (!$datas){
-        generate_error("Login and password don't match.");
+    if(!$datas){
+        generate_error("Login and password do not match.");
     }
     else{
         //Returning a JSON file that contains all the data
         Flight::json($datas);
     }
+}
+
+function get_hash_pwd($login, $password, $database){
+    /*  Gets the hashed version of the password the user entered using the salt saved in the database
+     *  @password : string that contains the user's password
+     *  @database : Medoo variable that contains connection data of the database
+     */
+    //Getting the salt saved in the database
+    $datas = $database->select("user",
+        ["salt"],
+        ["login"=>$login]);
+
+    if($datas == null){
+        return null;
+    }
+
+    //Concat the password and the salt
+    $hashed = $password.$datas[0]['salt'];
+    //Hashing the new string
+    $hashed = hash("sha512", $hashed);
+    return $hashed;
 }
--- functions/user_registration.php
+++ functions/user_registration.php
@@ -22,8 +22,23 @@ function user_registration_json($json, $database){
 
 
     if (!$datas){
+        //Verifying optionnal values in the JSON file
+        if(!isset($json->surname)){
+            $surname = null;
+        }
+        else{
+            $surname = $json->surname;
+        }
+
+        if(!isset($json->name)){
+            $name = null;
+        }
+        else{
+            $name = $json->name;
+        }
+
         //Inserting the new user in the DB
-        user_registration($json->login, $json->password, $json->mail, $json->birthdate, $database);
+        user_registration($json->login, $surname, $name, $json->password, $json->mail, $json->birthdate, $database);
     }
     else{
         //If the account already exists then sens the error
@@ -31,7 +46,7 @@ function user_registration_json($json, $database){
     }
 }
 
-function user_registration($login, $password, $mail, $birthdate, $database){
+function user_registration($login, $surname, $name, $password, $mail, $birthdate, $database){
     /*  Function that creates a new user in the database using his information passed in parameter.
      *  @login : string that contains the user's login
      *  @password : string that contains the user's password
@@ -39,34 +54,63 @@ function user_registration($login, $password, $mail, $birthdate, $database){
      *  @birthdate : date type variable that contains the user's date of birth
      *  @database : Medoo variable that contains connection data of the database
      */
+    //Generation of the salt
+    $salt = bin2hex(random_bytes(15));
+    //Verifying the length of the salt, if needed we resize it
+    if(strlen($salt)>30){
+        $salt = substr($salt, 0, 30);
+    }
+    //Generation of the hashed password using a concatenation of the password and the salt hashed with sha512
+    $secure = $password.$salt;
+    $hashed = hash("sha512", $secure);
+
     //We calculate the age of the person to define his initial user_type
     $age = date_diff(date_create($birthdate), date_create('today'))->y;
+    //Now we define the user's user_type
+    $type = null;
 
-    //Now we define the person's user_type
-
-    if($age <= 25){
-        //Student
-        $type = 2;
-    }
-    else if ($age >= 65){
-        //Senior
-        $type = 3;
-    }
-    else{
-        //Default
-        $type = 1;
+    if($age != null){
+        if($age <= 25){
+            //Young people
+            $type = get_id_from_user_type("Jeune", $database);
+        }
+        else if ($age >= 65){
+            //Senior
+            $type = get_id_from_user_type("Senior", $database);
+        }
+        else{
+            //Default
+            $type = get_id_from_user_type("Normal", $database);
+        }
     }
 
     $data = $database->insert("user", [
+        "id" => null,
+        "surname" => $surname,
+        "name" => $name,
         "login" => $login,
-        "hash_pwd" => $password,
+        "hash_pwd" => $hashed,
+        "salt" =>$salt,
         "mail" => $mail,
         "registration_date" => date("Y-m-d"),
+        "birthdate" => $birthdate,
         "user_type" => $type
     ]);
 
     //Verify the number of rows affected
     if($data->rowCount() <> 1){
-        generate_error($data->errorCode());
-    };
+        generate_error($data->errorInfo());
+    }
+}
+
+function get_id_from_user_type($name, $database){
+    /*  Gets the id of a user_type depending on its name
+     *  @name : string that contains the user_type's name
+     *  @database : Medoo variable that contains connection data of the database
+     */
+    $datas = $database->select("user_type",
+        ["id"],
+        ["name"=>$name]);
+
+    return $datas[0]['id'];
 }
