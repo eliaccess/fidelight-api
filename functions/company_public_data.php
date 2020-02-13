239bba89665b26d380b3bbb75dce3507b4f6496c
Elias Limouni <elias.limouni@gmail.com>
2020-02-13 15:53:55 +0100

    Completed functions that gets the compagnies public information :hammer:

--- functions/company_public_data.php
+++ functions/company_public_data.php
@@ -5,7 +5,8 @@
  *  Made by Elias LIMOUNI
  */
 
-function get_company_public_data($login, $database){
+function get_company_public_data($login, $database)
+{
     /*  Function that sends a JSON containing the profile of a person
      *  @login : string that contains the login of the person
      *  @database : Medoo variable that contains connection data of the database
@@ -14,20 +15,65 @@ function get_company_public_data($login, $database){
      */
     $datas = $database->select("company",
         [
-            "[>]company_type" => ["company_type"=>"id"]
-        ],[
+            "[>]company_type" => ["company_type" => "id"]
+        ], [
+            "company.id",
             "company.name",
             "company.login",
-            "company.phone",
+            "company.phone_number",
+            "company.phone_code",
             "company.registration_date",
             "company_type.name (company_type)"
-        ], ["login"=>$login]);
+        ], ["login" => $login]);
+
+    $id = $datas[0]['id'];
+    //Get all the locations of a selected company
+    $locations = $database->select("company_location",
+        [
+            "[>]company" => ["company" => "id"]
+        ], [
+            "company_location.id",
+            "company_location.phone_number",
+            "company_location.phone_code",
+            "company_location.gps_coordinates",
+            "company_location.street_number",
+            "company_location.street",
+            "company_location.city",
+            "company_location.country"
+        ], [
+            "company_location.company" => $id
+        ]);
+    //Creation of a new array to manage the links of the pictures of each location and rewriting it
+    $new_array=array();
+    foreach($locations as $key => $value){
+        //Get all the links of the pictures of a location
+        $picture = $database->select("company_location_picture",[
+                "company_location_picture.picture_link"
+            ],
+            [
+                "company_location" => $value['id']
+            ]);
+
+        //Remove the id from the request
+        unset($locations[$key]['id']);
+
+        //creating a temp_array
+        $temp_array = array();
+        foreach($picture as $key_picture => $link){
+            $temp_array[$key_picture+1] = $link['picture_link'];
+        }
+
+        //Add all the pictures of the location in the array
+        $new_array[$key+1]=array("location_information" => $locations[$key], "list_pictures" => $temp_array);
+    }
 
     if (!$datas){
         generate_error("This company does not exist.");
     }
     else{
+        //Add the adress to the company information
+        $full_datas = array("company_information" => $datas[0], "all_locations" => $new_array);
         //Returning a JSON file that contains all the data
-        Flight::json($datas);
+        Flight::json($full_datas);
     }
 }
