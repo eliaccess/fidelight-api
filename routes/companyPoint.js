498554bb2eb7b3a1392da52dc10270ebd487ea2b
Elias Limouni <elias.limouni@gmail.com>
2021-05-06 23:30:21 +0200

    Added a route to give points to a user

--- routes/companyPoint.js
+++ routes/companyPoint.js
@@ -144,8 +144,9 @@ router.delete('/api/company/points/', midWare.checkToken, (req, res, next) => {
 */
 
 
-router.get('/api/points/type', (req, res, next) => {
+router.get('/api/points/type', midWare.checkToken,(req, res, next) => {
     try {
+        validationResult(req).throw();
         db.query("SELECT * points_earning_type", (err, rows, result) => {
             if (err) {
                 res.status(410).jsonp(err);
@@ -160,10 +161,74 @@ router.get('/api/points/type', (req, res, next) => {
 });
 
 
+let usePts = [
+    check('qr_key').exists(),
+    check('points').exists(),
+    midWare.checkToken
+];
 
-router.post('/api/company/points/use/:usrId', cpyPoint, (req, res, next) => {
+router.post('/api/company/points/use/:usrId', usePts, (req, res, next) => {
     try {
-        
+        if(req.decoded.type != 'company'){
+            res.status(403).jsonp('Access forbidden');
+            return 2;
+        }
+        validationResult(req).throw();
+
+        let trData = {
+            user: req.params.usrId,
+            value: req.body.points,
+            seller: null,
+            discount: null,
+            date: new Date(),
+            company: req.decoded.id,
+            nb_used: 1,
+            money_saved: 0,
+        };
+        /* Creating the transaction */
+        db.query("INSERT INTO transaction SET ?", trData, (err, rows, results) => {
+            if (err) {
+                res.status(410).jsonp(err);
+                next(err);
+            } else {
+                /* Verify if the account already exists for the user in that company */
+                db.query("SELECT points FROM balance WHERE company = ? AND user = ?", [req.decoded.id, req.params.usrId], (err, rows2, results2) => {
+                    if(err){
+                        res.status(410).jsonp(err);
+                        next(err);
+                    } else {
+                        /* if it exists : adding the points, else creating the account and adding the amount of points */
+                        if(rows2[0]){
+                            let blcData = {
+                                points: (req.body.points + rows2[0].points)
+                            };
+                            db.query("UPDATE balance SET ? WHERE company = ? AND user = ?", [blcData, req.decoded.id, req.params.usrId], (err, rows3, results3) => {
+                                if(err){
+                                    res.status(410).jsonp(err);
+                                    next(err);
+                                } else {
+                                    res.status(200).jsonp({transaction: rows.insertId});
+                                }
+                            });
+                        } else {
+                            let blcData = {
+                                user: req.params.usrId,
+                                company: req.decoded.id,
+                                points: req.body.points
+                            };
+                            db.query("INSERT INTO balance SET ?", blcData, (err, rows3, results3) => {
+                                if(err){
+                                    res.status(410).jsonp(err);
+                                    next(err);
+                                } else {
+                                    res.status(200).jsonp({transaction: rows.insertId});
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        });
     } catch (err) {
         res.status(400).json(err);
     }
