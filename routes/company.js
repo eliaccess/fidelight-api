69576db4a17b57b63e692e8568ed968ecb03d1b8
Elias Limouni <elias.limouni@gmail.com>
2021-11-18 16:46:30 +0100

    Changing the limit of size of file location :construction:

--- routes/company.js
+++ routes/company.js
@@ -27,9 +27,7 @@ const fileFilter = (req, file, cb) => {
 const Multer = require("multer");
 const multer = Multer({
     storage: Multer.memoryStorage(),
-    limits: {
-      fileSize: 5 * 1024 * 1024, // no larger than 5mb, you can change as needed.
-    },
+    fileSize: 5 * 1024 * 1024, // no larger than 5mb, you can change as needed.
     fileFilter: fileFilter,
     filename: function(req, file, cb){
         cb(null, '/company/logo/' + new Date().toISOString() + file.originalname);
