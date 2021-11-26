a7ac1f02135ba31418a17f962fe161fe04834e2c
Elias Limouni <elias.limouni@gmail.com>
2021-11-26 19:10:01 +0100

    Correcting another await while function is not async :hammer:

--- routes/companyAuth.js
+++ routes/companyAuth.js
@@ -123,7 +123,7 @@ router.post('/v1/company/register', regValidate, async (req, res, next) => {
                                             }
                                             next(err);
                                         } else {
-                                            let emailToken = await getEmailToken(insertedId, 'company');
+                                            let emailToken = getEmailToken(insertedId, 'company');
                                             let linkConf = "https://api.fidelight.fr/company/verify/" + emailToken
                                             let content = await emailFunctions.generateConfirmationEmailCompany(req.body.name, linkConf);
                                             let mailOptions = await emailFunctions.generateEmailOptions(req.body.email, content);
